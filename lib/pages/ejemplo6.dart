dbdfca48e7899cf98d4f356862cbfb89d98d0d0d
JeniferDuran <144250210+JeniferDuran@users.noreply.github.com>
2023-11-13 09:57:20 -0600

    ejemplo2

---
+++ lib/pages/ejemplo6.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Ejemplo6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'Ejemplo6',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
