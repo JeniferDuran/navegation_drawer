8d38783645b2c2b73785e35671ae3c79424523a2
JeniferDuran <144250210+JeniferDuran@users.noreply.github.com>
2023-11-13 09:59:28 -0600

    actualizacion

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,32 +4,45 @@ import 'package:navegation_drawer/pages/ejemplo2.dart';
 import 'package:navegation_drawer/pages/ejemplo3.dart';
 import 'package:navegation_drawer/pages/ejemplo4.dart';
 import 'package:navegation_drawer/pages/ejemplo5.dart';
+import 'package:navegation_drawer/pages/ejemplo6.dart';
 import 'package:navegation_drawer/pages/inicio.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
+
 class _HomeState extends State<Home> {
   int _ItemDrawer = 0;
-   _getDrawerItem(int position){
-    switch(position){
-      case 0:return Inicio();
-      case 1:return Ejemplo1();
-      case 2:return Ejemplo2();
-      case 3:return Ejemplo3();
-      case 4:return Ejemplo4();
-      case 5:return Ejemplo5();
+  _getDrawerItem(int position) {
+    switch (position) {
+      case 0:
+        return Inicio();
+      case 1:
+        return Ejemplo1();
+      case 2:
+        return Ejemplo2();
+      case 3:
+        return Ejemplo3();
+      case 4:
+        return Ejemplo4();
+      case 5:
+        return Ejemplo5();
+        case 6:
+        return Ejemplo6();
+        
     }
   }
-  void _onSelectItemDrawer(int pos){
+
+  void _onSelectItemDrawer(int pos) {
     Navigator.pop(context);
-     setState(() {
-       _ItemDrawer = pos;
-     });
+    setState(() {
+      _ItemDrawer = pos;
+    });
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Navegation Drawer'),
@@ -52,51 +65,55 @@ class _HomeState extends State<Home> {
             ),
             ListTile(
               leading: Icon(Icons.house),
-              title: Text('home'
-
-              ),
-              onTap: (){
+              title: Text('home'),
+              onTap: () {
                 _onSelectItemDrawer(0);
               },
-              ),
-
-            Divider(
-              color: Colors.black
             ),
 
+            Divider(color: Colors.black),
+
             //ListTile(
-             // leading: Icon(Icons.arrow_forward_ios),
-             // title: Text('Ejemplo1'),
-             // onTap: (){
-               // _onSelectItemDrawer(1);
-             // },
-           // ),
+            // leading: Icon(Icons.arrow_forward_ios),
+            // title: Text('Ejemplo1'),
+            // onTap: (){
+            // _onSelectItemDrawer(1);
+            // },
+            // ),
             ListTile(
               leading: Icon(Icons.arrow_forward_ios),
               title: Text('LinearProgressIndicator'),
-              onTap: (){
+              onTap: () {
                 _onSelectItemDrawer(2);
               },
             ),
             ListTile(
               leading: Icon(Icons.arrow_forward_ios),
               title: Text('CircularProgressIndicator'),
-              onTap: (){
+              onTap: () {
                 _onSelectItemDrawer(3);
               },
             ),
             ListTile(
               leading: Icon(Icons.arrow_forward_ios),
               title: Text('Ejemplo4'),
-              onTap: (){
+              onTap: () {
                 _onSelectItemDrawer(4);
               },
             ),
             ListTile(
               leading: Icon(Icons.arrow_forward_ios),
               title: Text('Ejemplo5'),
-              onTap: (){
+              onTap: () {
                 _onSelectItemDrawer(5);
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.arrow_forward_ios),
+              title: Text('Ejemplo6'),
+              onTap: () {
+                _onSelectItemDrawer(6);
               },
             ),
           ],
